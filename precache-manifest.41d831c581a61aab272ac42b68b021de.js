159311e330cb93bfa557915bef488bd62f524a59
Khang Le <lekhang4497@gmail.com>
2018-10-27 22:39:19 +0700

    initialize

---
+++ precache-manifest.41d831c581a61aab272ac42b68b021de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "041b1e5c599771b7960d",
+    "url": "/ghem-website/static/js/runtime~main.041b1e5c.js"
+  },
+  {
+    "revision": "8388bb780fdc4919a350",
+    "url": "/ghem-website/static/js/main.8388bb78.chunk.js"
+  },
+  {
+    "revision": "405845da122cb1b8037c",
+    "url": "/ghem-website/static/js/1.405845da.chunk.js"
+  },
+  {
+    "revision": "8388bb780fdc4919a350",
+    "url": "/ghem-website/static/css/main.de8f17a5.chunk.css"
+  },
+  {
+    "revision": "405845da122cb1b8037c",
+    "url": "/ghem-website/static/css/1.221361d5.chunk.css"
+  },
+  {
+    "revision": "3d7d16250a4de16db7633e6c3f3c5c45",
+    "url": "/ghem-website/index.html"
+  }
+];
