f48441ed3e1e6780ba7066df1065481f062d7c75
Khang Le <lekhang4497@gmail.com>
2018-11-17 04:33:06 +0700

    finish layout

---
+++ precache-manifest.aeb98926e96cb1660d42141e605092f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "41de5b88c858eaa78bf5",
+    "url": "/static/js/main.41de5b88.chunk.js"
+  },
+  {
+    "revision": "28d3ae5013672c3fe876",
+    "url": "/static/js/1.28d3ae50.chunk.js"
+  },
+  {
+    "revision": "41de5b88c858eaa78bf5",
+    "url": "/static/css/main.b151d814.chunk.css"
+  },
+  {
+    "revision": "28d3ae5013672c3fe876",
+    "url": "/static/css/1.f30292b5.chunk.css"
+  },
+  {
+    "revision": "07bf13fc5cb928c16b17f1b8c7b075a7",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf1f1f25a8440755df7c5244931737e3.js"
+  "/precache-manifest.aeb98926e96cb1660d42141e605092f7.js"
 );
 
 workbox.clientsClaim();
